9b07d40b4bee04321eaabab331f8358b57b4d151
cancelban27 <95107475+cancelban27@users.noreply.github.com>
2021-11-27 06:22:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,16 @@
 const CONFIG = {
     introTitle: 'iuuuu à!',
-    introDesc: `Quốc có đôi lời muốn nói với iuuuu mong đọc cho`,
+    introDesc: `Quốc có đôi lời muốn nói mong iuuuu đọc cho`,
     btnIntro: 'click nó nhoa',
     title: 'Quốc đã iuuuu từ cái nhìn đầu đến nay mình cũng đã tìm hiểu được 11 tháng rồi ấy chứ 😙',
     desc: 'iuuuu có còn chút tình cảm với Quốc hog ? nếu còn iuuuu hãy tiếp tục nó nhé😙  ',
     btnYes: 'tiếp tục <33',
     btnNo: 'không tiếp tục :3',
-    question:'2 ngày qua là ngày Quốc suy nghĩ rất nhiều . Trong thời gian qua Quốc đã vô tâm , trầm tính , nói những lời không tôn trọng đối làm iuuuu phải buồn , bực rất nhiều do cái tính ích kỉ , cái tôi của Quốc làm iuuuu chịu đựng khoảng thời gian qua , cái tôi của Quốc quá lớn mỗi lần iuuuu có phàn nàn về Quốc thì Quốc chỉ biết im lặng không còn cởi mở như trước , ít dám có lời nói dẫn đến việc iuuuu đã hết tình cảm với Quốc như bây giờ , với lại Quốc đã thất hứa do cái thú vui của mih làm iuuuu thất vọng nhìu , thời gian Quốc không biết làm gì ngoài việc nói mồm hết nên nó quá khó cho Quốc , Quốc không được trưởng mà cứ thằng trẻ trâu , như 1 con robot ngày nào cũng spam 1 tin làm nản cũng chỉ mún 2 ta không phải im lặng mỗi ngày . 
-Quốc cũng biết tình cảm của mình không còn như trước mà Quốc đã không thể liên kết nó lại như trước do cứ ở nhà liên tục và việc  học đến gia đình nó làm Quốc áp lực liên tục. iuuuu là nguồn cảm hứng để Quốc giải tỏa những cái áp lực đó . iuuuu tuyệt như vậy mà Quốc lại không làm được hình mẫu lí tưởng của iuuuu
-Mong iuuuu còn chút tình cảm có thể cho Quốc cơ hội để khắc phục kiểm điểm lại bản thân có được không, hai mình tạo làm cảm giác xưa được hog, nha iuuuuu ,Quốc còn thương iuuuu nhiều lắm . Có thể quay lại với Quốc được hog <3
-Nếu iuuuu đã hết iuuuu có ko rep tin nhắn nhắn  
-',
-    btnReply: 'Gửi cho Quốc <3',
+    question:'Quốc biết iuuuu đã bùn phiền Quốc quá nhiều ',
+    btnReply: 'có thể cho tạ lỗi được hog <3',
     reply: 'Cho Quốc 1 cơ hội nhóe <33333333',
     mess: 'được hong',
     messDesc: 'nếu được tối nay quốc có thể call với iuuuu được hog?.',
-    btnAccept: 'nếu được iuuuu nói cho Quốc nhóe , còn hong iuuuu có thể thoát khỏi trang này',
+    btnAccept: 'đây là những gì Quốc đã suy nghĩ 2 ngày qua',
     messLink: 'https://www.facebook.com/Ba0Qu0cthw/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
